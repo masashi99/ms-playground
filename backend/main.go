fcf1039332fe55d131413dc79d54085722287b0e
masashi99 <sgmdq601@gmail.com>
2025-02-02 21:01:33 +0900

    feat: 疎通確認まで

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
+	engine.Use(cors.New(
+		cors.Config{
+			AllowOrigins: []string{"http://localhost:3000"},
+		},
+	))
 	engine.GET("/api/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
--- frontend/src/features/MainPage/MainPageContainer.tsx
+++ frontend/src/features/MainPage/MainPageContainer.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
 import { MainPagePresentation } from "./MainPagePresentation";
 
-export async function MainPageContainer() {
-  const res = await fetch("http://localhost:8080/api/ping");
-  const data = await res.json();
-  return <MainPagePresentation message={data.message} />;
+export function MainPageContainer() {
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/api/ping");
+        const data = await res.json();
+        setData(data);
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetcher();
+  }, []);
+  if (loading) return null;
+  return <MainPagePresentation message={data?.message || ""} />;
 }
